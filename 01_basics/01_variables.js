a8054bd3c625b49dd4306a3d085d33a145dafcf6
Aditya Pratap Singh <158612122+AdityaSinghDevs@users.noreply.github.com>
2024-07-27 12:01:13 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accId = 12234
+let accEmail = "aditya@gmail.com"
+var accCity = "delhi"
+accPass= 22334455
+
+console.table([accId, accEmail, accCity, accPass]);
+
+accEmail= "lolchanged@gmail.com"
+accCity = "bangalore"
+accPass = 12345
+
+console.table([accId, accEmail, accCity, accPass]);
+
+// Now
+// Don't use 'var' and/or the no keywoard variable declaration like in 'accPass'
+// due to block scope error
+
+//only use 'let'
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"// Treat all JS code as newer version
+
+// alert(3+3) not using browser, using node
+
+console.log(3+3)
+console.log("Aditya");
+
+//instead of separating by ; use proper readbility indenting
+
+//datatypes
+
+let name = "Aditya"
+let age = 21
+let isloggedin = false
+
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true or false
+// null => standalone value 
+// undefined
+//symbol
+
+
+//objects
+
+console.log(typeof name)
+
+console.log(typeof null); //null is considered object
+console.log(typeof undefined); //considered undefined 
+
+
