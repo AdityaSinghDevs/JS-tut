fc95eec6c34429d7c3446549c36017ac2e284f01
Aditya Pratap Singh <158612122+AdityaSinghDevs@users.noreply.github.com>
2024-07-30 20:30:51 +0000

    03.1

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -38,4 +38,44 @@ numberinstring= String(somenumber)
 console.log(somenumber);
 console.log(typeof numberinstring);
 
-//end
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);// prints neg value
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3); // simple operations
+
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2
+console.log(str3); //concatenates them
+
+console.log("1" + 2);//outputs 12
+console.log(1 + "2");// outputs 12
+//but
+console.log("1" + 2 + 2);//outputs 122
+console.log(1 + 2 + "2"); //output is 32
+// uses the type conversion acc to the first value
+
+console.log( (3 + 4) * 5 % 3); //use paranthesis
+
+console.log(+true);// same but for boolean
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2// stupid and unreadable code 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
